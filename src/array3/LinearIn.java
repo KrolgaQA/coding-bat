cd165399c20f8cc79cd144945faa58f7c4ed8f87
Olga Krasilnikova <ol.kras.andr@gmail.com>
2018-10-31 11:24:42 +0300

    array3 solutions

---
+++ src/array3/LinearIn.java
@@ -0,0 +1,20 @@
+package array3;
+
+public class LinearIn {
+    public boolean linearIn(int[] outer, int[] inner)
+    {
+        boolean notFound;
+        for(int inI = 0, outI = 0; inI < inner.length; inI++)
+        {
+            notFound = true;
+            for(; outI < outer.length && notFound; outI++)
+            {
+                if(inner[inI] == outer[outI])
+                    notFound = false;
+            }
+            if(notFound)
+                return false;
+        }
+        return true;
+    }
+}
---
+++ src/array3/SquareUp.java
@@ -0,0 +1,16 @@
+package array3;
+
+public class SquareUp {
+    public int[] squareUp(int n)
+    {
+        int[] arr = new int[n*n];
+        int p;
+        for(int i = 1; i <= n; i++)
+        {
+            p = n * i - 1;
+            for(int j = 1; j <= i; j++, p--)
+                arr[p] = j;
+        }
+        return arr;
+    }
+}
